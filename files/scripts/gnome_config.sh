1a200e3ffc4ad82d18f115d30fcc6c2081248855
SuperGabe5 <148593937+SuperGabe5@users.noreply.github.com>
2025-10-13 12:04:10 -0400

    Add files via upload

---
+++ files/scripts/gnome_config.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+# Your code goes here.
+echo 'Gnome Configurer v0.0.1'
+echo 'this tweaks things to be more like ChromiumOS'
